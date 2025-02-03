37123ca1143cc7e64fd5a2c0111ef6f5f0387d5d
MiloszKlaput <klaputmilosz@gmail.com>
2025-02-03 20:46:37 +0100

    Dodanie serwisu z danymi

--- pomelo.fe/src/app/services/jira-data.service.ts
+++ pomelo.fe/src/app/services/jira-data.service.ts
@@ -3,4 +3,33 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class JiraApiService { }
+export class JiraDataService {
+  private boardId: number | null = null;
+  private sprintsIds: string[] | null = null;
+  private epicsIds: string[] | null = null;
+
+  setBoardId(id: number): void {
+    this.boardId = id;
+  }
+
+  getBoardId(): number | null {
+    return this.boardId;
+  }
+
+  setSprintsIds(ids: string[]): void {
+    this.sprintsIds = [...ids];
+  }
+
+  getSprintsIds(): string[] | null {
+    return this.sprintsIds ? [...this.sprintsIds] : null;
+  }
+
+  setEpicsIds(ids: string[]): void {
+    this.epicsIds = [...ids];
+  }
+
+  getEpicsIds(): string[] | null {
+    return this.epicsIds ? [...this.epicsIds] : null;
+  }
+}
+

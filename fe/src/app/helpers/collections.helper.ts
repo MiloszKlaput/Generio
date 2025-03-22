64ee16cf7e33f3ec295bb3ecbbbac392f7e584fb
MiloszKlaput <klaputmilosz@gmail.com>
2025-03-22 14:47:22 +0100

    Dodanie algorytmu tasującego array

---
+++ fe/src/app/helpers/collections.helper.ts
@@ -0,0 +1,9 @@
+// Fisher-Yates Shuffle
+export class CollectionsHelper {
+  static shuffleArray(array: any[]): void {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+}
